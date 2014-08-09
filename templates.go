23ca8169e57e2301c3ce214bfe1167e9fa479836
Jun Kimura <jksmphone@gmail.com>
2014-08-09 22:04:09 +0900

    Escape html in template.

--- templates.go
+++ templates.go
@@ -5,18 +5,18 @@ import (
 )
 
 const defaultTemplates = `
-{{define "TextTypeField"}}<input type="text" name="{{.Field.GetName}}" value="{{.Value}}"></input>{{end}}
+{{define "TextTypeField"}}<input type="text" name="{{.Field.GetName}}" value="{{.Value | html}}"></input>{{end}}
 
 {{define "TextWidget"}}<input type="text" name="{{.Name}}" value=""{{range $attr, $val := .Attrs}} {{$attr}}="{{$val}}"{{end}}></input>{{end}}
 
 {{define "SelectWidget"}}<select {{range $attr, $val := .Attrs}}{{$attr}}="{{$val}}"{{end}}>
-{{range $idx, $val := .Options}}<option value="{{$val.Value}}">{{$val.Label}}</option>
+{{range $idx, $val := .Options}}<option value="{{$val.Value | html}}">{{$val.Label}}</option>
 {{end}}</select>{{end}}
 
-{{define "RadioWidget"}}{{ $name := .Name}}{{range $idx, $val := .Options}}<input type="radio" name="{{$name}}" value="{{$val.Value}}"{{if $val.Checked }} checked{{end}}{{if $val.Disabled}} disabled{{end}}>{{$val.Label}}
+{{define "RadioWidget"}}{{ $name := .Name}}{{range $idx, $val := .Options}}<input type="radio" name="{{$name}}" value="{{$val.Value | html}}"{{if $val.Checked }} checked{{end}}{{if $val.Disabled}} disabled{{end}}>{{$val.Label}}
 {{end}}{{end}}
 
-{{define "CheckboxWidget"}}{{ $name := .Name}}{{range $idx, $val := .Options}}<input type="checkbox" name="{{$name}}" value="{{$val.Value}}"{{if $val.Checked }} checked{{end}}{{if $val.Disabled}} disabled{{end}}>{{$val.Label}}
+{{define "CheckboxWidget"}}{{ $name := .Name}}{{range $idx, $val := .Options}}<input type="checkbox" name="{{$name}}" value="{{$val.Value | html}}"{{if $val.Checked }} checked{{end}}{{if $val.Disabled}} disabled{{end}}>{{$val.Label}}
 {{end}}{{end}}
 `
 
