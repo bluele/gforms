604d7a7b5a8171407e471afb21557db5196f8304
Jun Kimura <jksmphone@gmail.com>
2014-08-10 22:32:08 +0900

    Added example for radio_input.

---
+++ examples/radio_input.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bluele/gforms"
+	"net/http"
+)
+
+type Lang struct {
+	Name string `gforms:"name"`
+}
+
+func main() {
+	formTpl := `<form method="post">%v<input type="submit"></form>`
+	langForm := gforms.DefineModelForm(Lang{}, gforms.NewFields(
+		gforms.NewTextField(
+			"name",
+			gforms.Validators{
+				gforms.Required(),
+			},
+			gforms.NewRadioWidget(
+				map[string]string{},
+				func() gforms.RadioOptions {
+					return gforms.StringRadioOptions([][]string{
+						{"Golang", "golang", "false", "false"},
+						{"Python", "python", "false", "false"},
+						{"C", "c", "false", "true"},
+					})
+				},
+			),
+		),
+	))
+	http.HandleFunc("/lang", func(w http.ResponseWriter, r *http.Request) {
+		form := langForm(r)
+		if r.Method != "POST" {
+			w.Header().Set("Content-Type", "text/html")
+			fmt.Fprintf(w, fmt.Sprintf(formTpl, form.Html()))
+			return
+		}
+		if form.IsValid() {
+			obj := form.GetModel()
+			fmt.Fprintf(w, "%v <=> %v", form.CleanedData, obj)
+		} else {
+			fmt.Fprintf(w, "%v", form.Errors)
+		}
+	})
+
+	http.ListenAndServe(":9000", nil)
+}
