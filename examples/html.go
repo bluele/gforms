6aa21d618c83f72338a13646c435b98d6a78158c
Jun Kimura <jksmphone@gmail.com>
2014-08-28 15:46:10 +0900

    Fixed example codes.

--- examples/html.go
+++ examples/html.go
@@ -17,6 +17,7 @@ func main() {
 			"name",
 			gforms.Validators{
 				gforms.Required(),
+				gforms.MinLengthValidator(3),
 				gforms.MaxLengthValidator(32),
 			},
 		),
@@ -27,6 +28,7 @@ func main() {
 	))
 
 	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
 		form := userForm(r)
 		if r.Method != "POST" {
 			fmt.Fprintf(w, form.Html())
@@ -37,12 +39,13 @@ func main() {
 			form.MapTo(&user)
 			fmt.Fprintf(w, "%v", user)
 		} else {
-			for k, f := range form.Fields.GetMap() {
-				err, hasErr := form.Errors[k]
+			for _, f := range form.Fields.GetList() {
+				name := f.GetName()
+				err, hasErr := form.Errors[name]
 				if hasErr {
-					fmt.Fprintf(w, "<label>%v</label><div class=\"error\">%v</div>%v\n", k, err, f.Html(form.RawData))
+					fmt.Fprintf(w, "<label>%v</label><div class=\"error\">%v</div>%v\n", name, err, f.Html(form.RawData))
 				} else {
-					fmt.Fprintf(w, "<label>%v</label>%v\n", k, f.Html(form.RawData))
+					fmt.Fprintf(w, "<label>%v</label>%v\n", name, f.Html(form.RawData))
 				}
 			}
 		}
